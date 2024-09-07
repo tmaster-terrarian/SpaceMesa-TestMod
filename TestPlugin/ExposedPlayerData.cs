ff56ddaf51f4946e5a17333739267a1c7def0ddf
bscit <54488066+tmaster-terrarian@users.noreply.github.com>
2024-09-07 09:24:07 -0700

    ExposedPlayerData

--- TestPlugin/ExposedPlayerData.cs
+++ TestPlugin/ExposedPlayerData.cs
@@ -1,6 +1,43 @@
+using System.Reflection;
+using UnityEngine;
+
 namespace TestPlugin;
 
-public class ExposedPlayerData
+public class ExposedPlayerData : MonoBehaviour
 {
-    
+    private static readonly FieldInfo isJumpingField = typeof(PlayerController).GetField("isJumping", BindingFlags.Instance | BindingFlags.NonPublic);
+
+    private bool _isGrounded;
+
+    public bool IsGrounded => _isGrounded;
+
+    public bool IsJumping {
+        get {
+            var playerController = gameObject.GetComponent<PlayerController>();
+            if(playerController is null) return false;
+
+            return (bool)isJumpingField.GetValue(playerController);
+        }
+    }
+
+    private void OnEnable()
+    {
+        On.PlayerController.isGrounded += PlayerController_isGrounded;
+    }
+
+    private void OnDisable()
+    {
+        On.PlayerController.isGrounded -= PlayerController_isGrounded;
+    }
+
+    // Get isGrounded
+    private static bool PlayerController_isGrounded(On.PlayerController.orig_isGrounded orig, PlayerController self)
+    {
+        var component = self.gameObject.GetComponent<ExposedPlayerData>();
+
+        if(component is null)
+            return orig(self);
+
+        return component._isGrounded = orig(self);
+    }
 }
--- TestPlugin/Plugin.cs
+++ TestPlugin/Plugin.cs
@@ -12,26 +12,14 @@ public class Plugin : BaseUnityPlugin
     public const string PLUGIN_NAME = "Test Plugin";
     public const string PLUGIN_VERSION = "1.0.0";
 
-    private static GameObject playerObj;
-    private static PlayerController PlayerController => playerObj.GetComponent<PlayerController>();
-
-    private static GameObject cube;
+    private static readonly List<GameObject> cubes = [];
 
     private void OnEnable()
     {
         Logger.Log(LogLevel.Info, "Hello World!");
 
-        On.PlayerController.isGrounded += OnPlayerController_isGrounded;
         On.PlayerController.Start += OnPlayerController_Start;
         On.PlayerController.Jump += OnPlayerController_Jump;
-    }
-
-    private static bool playerIsGrounded = false;
-
-    private bool OnPlayerController_isGrounded(On.PlayerController.orig_isGrounded orig, PlayerController self)
-    {
-        playerIsGrounded = orig(self);
-        return playerIsGrounded;
     }
 
     private void OnPlayerController_Start(On.PlayerController.orig_Start orig, PlayerController self)
@@ -39,12 +27,15 @@ public class Plugin : BaseUnityPlugin
         orig(self);
         Logger.Log(LogLevel.Info, "Player Started!");
 
-        playerObj = self.gameObject;
+        var playerObj = self.gameObject;
+        playerObj.AddComponent<ExposedPlayerData>();
 
-        cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube.transform.localScale = Vector3.one * 0.25f;
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.localScale = Vector3.one * 0.5f;
         cube.transform.position = playerObj.transform.position + (Vector3.back * 1) + (Vector3.up * 1.35f);
         cube.transform.SetParent(playerObj.transform, true);
+
+        cubes.Add(cube);
 
         Physics2D.gravity = new(0, -9.8f);
     }
@@ -55,13 +46,11 @@ public class Plugin : BaseUnityPlugin
         self.rb.velocity = new(self.rb.velocity.x, 40);
     }
 
-    private void Start()
-    {
-        
-    }
-
     private void Update()
     {
-        cube?.transform.Rotate(10 * Time.deltaTime, -3 * Time.deltaTime, 12 * Time.deltaTime);
+        foreach(var cube in cubes)
+        {
+            cube?.transform?.Rotate(10 * Time.deltaTime, -3 * Time.deltaTime, 12 * Time.deltaTime);
+        }
     }
 }
